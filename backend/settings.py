f8825ea5941cd30ce7577fefb6c97ee104106f29
user <caarroba@espol.edu.ec>
2025-01-06 10:30:42 -0500

    stop 3

--- backend/settings.py
+++ backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'base_django'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
